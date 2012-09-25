fdac1bbf77e9fdab9f1129d2de103b7e7a2b3006
Yoav Shapira <yoavs@yoavshapira.com>
2012-09-25 18:45:41 -0400

    add #11

--- euler9.py
+++ euler9.py
@@ -3,26 +3,14 @@ Project Euler script #9.
 """
 import math
 
-def is_triplet(a, b, c):
-	print "Testing %d, %d, %d" % (a, b, c)
-	return a*a + b*b == c*c
-
 def main():
-	a = 1
-	b = 2
-	c = 3
-	desired_triplet = []
+	limit = 1000
+	for a in range(1, limit + 1):
+		for b in range(1, limit + 1):
+			c = 1000 - b - a
+			if a * a + b * b == c * c:
+				return a * b * c
 
-	while (a+b+c <= 1000):
-		if (is_triplet(a,b,c)):
-			print "Triplet!!! %d, %d, %d" % (a, b, c)
-			if (a + b + c == 1000):
-				desired_triplet = [a, b, c]
-		a += 1
-		b += 1
-		c += 1
-	
-	print "Done!  Desired triplet: %s" % Desired_triplet
 
 if __name__ == "__main__":
-    main()
+    print main()
